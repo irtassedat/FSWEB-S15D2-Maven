93c5019f609640c39c688ef47a4c6cb88e501b75
irtassedat <sedatirtas.1@gmail.com>
2024-09-05 03:44:14 +0300

    solve

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,7 +1,65 @@
 package org.example;
 
+import org.example.entity.*;
+
+import java.util.HashSet;
+import java.util.Set;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        System.out.println("-------------------------------------------------------------");
+
+//        Set<Task> annsTasks = new HashSet<>();
+//        Task taskAnn = new Task("workintech", "dummy dec", "ann", Status.IN_PROGRESS, Priority.HIGH);
+//        Task taskAnn2 = new Task("workintech", "dummy dec 2", "ann", Status.IN_PROGRESS, Priority.HIGH);
+//
+//        annsTasks.add(taskAnn);
+//        annsTasks.add(taskAnn2);
+//
+//        Set<Task> bobsTasks = new HashSet<>();
+//        Task taskbob = new Task("workintech", "dummy dec 3", "bob", Status.IN_PROGRESS, Priority.HIGH);
+//        Task taskbob2 = new Task("workintech", "dummy dec 4", "bob", Status.IN_PROGRESS, Priority.HIGH);
+//
+//        bobsTasks.add(taskbob);
+//        bobsTasks.add(taskbob2);
+//
+//        Set<Task> carolsTasks = new HashSet<>();
+//        Task taskcarol = new Task("workintech", "dummy dec 5", "carol", Status.IN_PROGRESS, Priority.HIGH);
+//        Task taskcarol2 = new Task("workintech", "dummy dec 6", "carol", Status.IN_PROGRESS, Priority.HIGH);
+//
+//        carolsTasks.add(taskcarol);
+//        carolsTasks.add(taskcarol2);
+//
+//        Set<Task> unassignedTasks = new HashSet<>();
+//        Task taskunassigned = new Task("workintech", "dummy dec 77", null, Status.IN_PROGRESS, Priority.HIGH);
+//
+//        unassignedTasks.add(taskunassigned);
+//// bur 37 deki satır gelmedi. neden?
+//
+//        TaskData taskData = new TaskData(annsTasks, bobsTasks, carolsTasks, unassignedTasks);
+//
+//        System.out.println("anntasks" + taskData.getTasks("ann"));
+//        System.out.println("bobtasks" + taskData.getTasks("bob"));
+//        System.out.println("alltasks" + taskData.getTasks("all"));
+//
+//        System.out.println("-------------------------------------------------------------");
+//
+//
+//        System.out.println("unassigned" + taskData.getDifferences(unassignedTasks,taskData.getTasks("ann")));
+//
+//
+//        System.out.println("-------------------------------------------------------------");
+//
+//        System.out.println("Farklı task var mı?" + taskData.getIntersection(annsTasks,bobsTasks));
+
+//        System.out.println("**********-----------****************---------------*************");
+//
+//        StringSet.findUniqueWords();
+//
+//        System.out.println("-------------------------------------------------------------");
+
+
+
+
     }
 }
